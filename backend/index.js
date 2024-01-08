6b7c27b71e12d94e4075b644d97d34e8975540f1
PiyushBadhe <badhepiyuraj1997@gmail.com>
2024-01-08 21:43:39 +0530

    Created get and post route for adding and fetching details of the books

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,10 @@
 import express from "express";
 import { PORT, mongodbURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.status(234).send('Started');
-});
+app.use(express.json());
 
 mongoose
     .connect(mongodbURL)
@@ -18,3 +17,46 @@ mongoose
     .catch((error) => {
         console.log('Error occurred:', error);
     });
+
+app.get('/', (req, res) => {
+    return res.status(234).send('Started');
+});
+
+app.post('/new-book', async (req, res) => {
+    try {
+        if (!req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear) {
+            return res.status(400).send({
+                message: 'Provide all required input fields: title, author and publish year',
+            });
+        }
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+        return res.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message });
+    }
+})
+
+app.get('/get-books', async (req, res) => {
+    try {
+        const books = await Book.find({});
+
+        return res.status(200).json({
+            total_books : books.length,
+            books_details: books,
+        });
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message });
+    }
+})
--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -20,4 +20,4 @@ const bookSchema = mongoose.Schema(
     },
 )
 
-const Book = mongoose.model('Book', bookSchema);
+export const Book = mongoose.model('Book', bookSchema);
