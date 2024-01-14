0248dad1d5c7ced6b34a5dbf50f4059c56313418
PiyushBadhe <badhepiyuraj1997@gmail.com>
2024-01-14 18:35:27 +0530

    created basic react layout using vite, also set up tailwind css stylings for react

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use('/books', bookRoutes);
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:5555',
+    origin: `http://localhost:${PORT}`,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
 }));
